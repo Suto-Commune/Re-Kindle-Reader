9fee00bea2ff8ac10fb3a2b8b4a6cc566c618a2f
hsn <hsn1919810@gmail.com>
2023-07-17 07:43:44 +0800

    更新java版本检查;更好的异常处理.

--- src/getready.py
+++ src/getready.py
@@ -1,3 +1,9 @@
+from pathlib import Path
+from urllib.error import HTTPError
+
+from src.utils.text import compare_version, is_ver_str
+
+
 def get():
     import httpx
     import wget
@@ -7,38 +13,62 @@ def get():
     import subprocess
 
     logging.info("Checking the ENV.")
+
+    # Create the not exist folder.
     if not os.path.exists("reader"):
         os.mkdir("reader")
-    else:
-        ...
+
     if not os.path.exists("./reader/RKR"):
         os.mkdir("./reader/RKR")
-    else:
-        ...
 
-    if not "jar" in str(os.listdir("./reader")):
+    # Check reader.jar is exist or not.
+    if not Path('./reader/reader.jar').exists():
         logging.warning("Reader not found.Try to download it from github.")
+
+        # Get the latest version.
         latest = httpx.get("https://api.github.com/repos/hectorqin/reader/releases/latest").json()
-        url = str()
-        for i in latest["assets"]:
-            if ".jar" in i["name"]:
-                url = i["browser_download_url"]
-                break
         try:
+            # Try to get the first `jar` file.
+            # In default, the first `jar` file is the reader.jar.
+            url = next(filter(lambda x: ".jar" in x["name"], latest["assets"]))["browser_download_url"]
+        except (KeyError, StopIteration):
+            logging.error(
+                "Cannot get the reader file.Try to download it from rope("
+                "https://github.com/hectorqin/reader/releases/latest).")
+            sys.exit(0)
+
+        try:
+            # Try to download the reader.jar.
             wget.download(url, "./reader/reader.jar")
-            print("")
-        except:
+        except HTTPError:
             logging.warning("Cannot download the reader file.Try to download it from ghproxy.")
             try:
+                # Try to download the reader.jar from ghproxy.
                 wget.download(f"https://ghproxy.com/{url}", "./reader/reader.jar")
-                print("")
-            except:
-                logging.error(f"Cannot download the reader file.Try to download it from {url}"
-                              f" by yourself,then rename it as \"reader.jar\",put it into the reader folder.")
+            except HTTPError:
+                logging.error(f'Cannot download the reader file.Try to download it from {url}'
+                              f' by yourself,then rename it as "reader.jar",put it into the reader folder.')
                 sys.exit(0)
+    try:
+        # Run `java --version` to check the java version.
+        output = subprocess.check_output(['java', '--version'], stderr=subprocess.STDOUT)
+    except FileNotFoundError:
+        logging.error('Java Not Found.Please Install Java 17+.You Can Download It From "https://adoptium.net"')
+        sys.exit(0)
 
-    if "java" not in str(subprocess.run(["where", "java"], stdout=subprocess.PIPE).stdout):
-        logging.error("Java Not Found.Please Install Java 17+.You Can Download It From adoptium.net")
+    # Get first line of the output.
+    info_list = output.decode('utf-8').strip().split('\n').pop(0).split(' ')
+
+    # Get first version string.
+    version_str = next(filter(lambda x: is_ver_str(x), info_list))
+
+    # Compare the version.
+    if not compare_version(version_str, '17.0.0'):
+        logging.error('Java version is too low.Please Install Java 17+.You Can Download It From "https://adoptium.net"')
         sys.exit(0)
 
     logging.info("Done.")
+
+
+if __name__ == '__main__':
+    ...
