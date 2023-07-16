ef9afca5fec0e1b8074cff9a585afa09149b86c4
Roren <RorenNatsumi@outlook.com>
2023-07-17 00:19:07 +0800

    更正主页失效的bug

--- src/pages/shelf/book_info/book_info.py
+++ src/pages/shelf/book_info/book_info.py
@@ -2,11 +2,11 @@ from flask import Blueprint, request
 from flask import render_template as template
 import src.book as book
 
-name = "bookinfo"
+name = "book_info"
 page = Blueprint(name, __name__, template_folder=".\\files")
 
 
-@page.route("/shelf/bookinfo", methods=['GET', 'POST'])
+@page.route("/shelf/book_info", methods=['GET', 'POST'])
 def bookshelf():
     data = request.args
     return data
